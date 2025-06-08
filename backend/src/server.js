658cd226287004bc57b14b72cf1e7638732a80eb
Chirag <chiragkhati1991@gmail.com>
2025-06-08 12:02:38 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send("working ");
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {  console.log(`Server is running on port ${PORT}`);  });
